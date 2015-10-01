ccacc526957a2617569db23a412f7218f6bf76ea
Lecomte Emerick <emerick.lecomte@gmail.com>
2015-10-01 11:46:34 +0200

    start analyze real time

--- SoundAnalyzer/MusicPlayer.cpp
+++ SoundAnalyzer/MusicPlayer.cpp
@@ -4,7 +4,7 @@
 
 void MusicPlayer::Start()
 {
-	FMOD::System_Create(&MusicPlayer::system);
+	/*FMOD::System_Create(&MusicPlayer::system);
 	this->sys->init(2, FMOD_INIT_STREAM_FROM_UPDATE, NULL);
-	FMOD_RESULT res = sys->createSound(soundPath, FMOD_, NULL, &this->Sound);
+	FMOD_RESULT res = sys->createSound(soundPath, FMOD_, NULL, &this->Sound);*/
 }
